f48d7d9996268235d2e9d5600801e347dad78218
no ha <61463030+goalandthink@users.noreply.github.com>
2022-01-30 15:29:30 +0800

    1

--- Assets/script/PVP/victory_net.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Photon.Pun;
-public class victory_net : MonoBehaviour
-{
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# SUSTech_OOAD_Pac_Man
-this repository is for sustech ooad course pac_man project
